1f993d32beef097f1fa0d35b20407deb78a1a678
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-16 22:41:47 +0200

    Bumped version

--- environments_utils/__version__.py
+++ environments_utils/__version__.py
@@ -1,2 +1,2 @@
 """Current version of package environments_utils"""
-__version__ = "1.0.3"
+__version__ = "1.0.4"
--- environments_utils/is_architecture.py
+++ environments_utils/is_architecture.py
@@ -4,7 +4,7 @@ import platform
 
 def is_arm() -> bool:
     """Return whether the current computer has ARM architecture."""
-    return platform.machine() == "aarch64"
+    return platform.machine() in ("aarch64", "arm64")
 
 
 def is_x86() -> bool:
