1e869ffe049f5be890283f2d19a8279851c9281b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-15 21:36:43 +0200

    Fixed typo

--- environments_utils/__init__.py
+++ environments_utils/__init__.py
@@ -4,7 +4,7 @@ from .is_tmux import is_tmux
 from .is_stdout_enabled import is_stdout_enabled
 from .is_architecture import is_x86, is_x86_64, is_arm
 from .is_os import is_linux, is_macos, is_windows, is_macos_with_arm
-from is_cluster import is_slurm_node, must_be_in_slurm_node, get_slurm_node_id, get_number_of_available_slurm_nodes
+from .is_cluster import is_slurm_node, must_be_in_slurm_node, get_slurm_node_id, get_number_of_available_slurm_nodes
 
 __all__ = [
     "is_notebook",
