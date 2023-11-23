cd602cea829b42f97211eb537d98121eb826251b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-11-23 15:12:24 +0100

    Updated pylint

--- environments_utils/is_colab.py
+++ environments_utils/is_colab.py
@@ -4,7 +4,7 @@
 def is_colab() -> bool:
     """Return a boolean representing if script is running within a jupyter notebook on Google COLAB."""
     try:
-        import google.colab  # pylint: disable=import-outside-toplevel
+        import google.colab  # pylint: disable=import-outside-toplevel unused-import import-error no-name-in-module
 
         return True
     except:  # pylint: disable=bare-except
